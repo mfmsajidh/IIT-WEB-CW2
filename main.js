f5356af5db77486fbe8db6bacc6203c978af9b7b
Madusha <madusha.2018828@iit.ac.lk>
2018-12-12 12:17:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ var sticky = navbar.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
-  } else {
+  }
+    else {
     navbar.classList.remove("sticky");
+    
+  }
+  if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+    document.getElementById('navbar').style.display='none';
+    
+  }
+  else{
+    document.getElementById('navbar').style.display='';
   }
 }
